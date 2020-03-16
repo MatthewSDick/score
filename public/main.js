19e27e183eeaef19825fcbc90a69d412e154756e
MatthewSDick <matthew.dick@outlook.com>
2020-03-16 12:23:50 -0400

    Explorer Part 1

--- public/main.js
+++ public/main.js
@@ -1,7 +1,27 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  console.log('Hello, World')
+}
+
+// Assign click events to the update buttons to update the corresponding values on the page.
+// The score buttons should adjust the current score by a "hard-coded" value.
+const changeTeam1Score = () => {
+  const newScore = document.querySelector('.team-1-input').value
+  console.log(newScore)
+  document.querySelector('.team-1-score').textContent = newScore
+}
+
+const changeTeam2Score = () => {
+  const newScore = document.querySelector('.team-2-input').value
+  console.log(newScore)
+  document.querySelector('.team-2-score').textContent = newScore
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', changeTeam1Score)
+
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', changeTeam2Score)
