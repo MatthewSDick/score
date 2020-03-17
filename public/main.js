ee183e6b61260b2a64539559fec4f5970b276d78
MatthewSDick <matthew.dick@outlook.com>
2020-03-16 22:02:22 -0400

    Adventure

--- public/main.js
+++ public/main.js
@@ -18,10 +18,15 @@ const team1AddPoint = () => {
   const newScore = parseInt(currScore) + 1
   console.log(newScore)
   document.querySelector('.team-1-score').textContent = newScore
-
+  updateScoreBoard()
   if (newScore > 20) {
     winner(1)
   }
+}
+
+const endPeriod = () => {
+  document.querySelector('.team-1-score').textContent = 0
+  document.querySelector('.team-2-score').textContent = 0
 }
 
 const team2AddPoint = () => {
@@ -30,7 +35,7 @@ const team2AddPoint = () => {
   const newScore = parseInt(currScore) + 1
   console.log(newScore)
   document.querySelector('.team-2-score').textContent = newScore
-
+  updateScoreBoard()
   if (newScore > 20) {
     winner(2)
   }
@@ -46,6 +51,7 @@ const team1SubtractPoint = () => {
   } else {
     document.querySelector('.team-1-score').textContent = newScore
   }
+  updateScoreBoard()
 }
 
 const team2SubtractPoint = team => {
@@ -58,6 +64,7 @@ const team2SubtractPoint = team => {
   } else {
     document.querySelector('.team-2-score').textContent = newScore
   }
+  updateScoreBoard()
 }
 
 const winner = team => {
@@ -75,7 +82,58 @@ const reset = () => {
   document.querySelector('.team-2-subtract-1-button').disabled = false
   document.querySelector('.team-1-score').textContent = 0
   document.querySelector('.team-2-score').textContent = 0
+  document.querySelector('.home1').textContent = 0
+  document.querySelector('.home2').textContent = 0
+  document.querySelector('.home3').textContent = 0
+  document.querySelector('.homef').textContent = 0
+  document.querySelector('.visit1').textContent = 0
+  document.querySelector('.visit2').textContent = 0
+  document.querySelector('.visit3').textContent = 0
+  document.querySelector('.visitf').textContent = 0
   document.querySelector('h1').textContent = 'Game in progress'
+}
+
+const updateScoreBoard = () => {
+  const homeScore = document.querySelector('.team-1-score').textContent
+  const visitorScore = document.querySelector('.team-2-score').textContent
+  const currPeriod = document.querySelector('[name="period"]:checked').value
+  console.log(currPeriod)
+
+  switch (currPeriod) {
+    case '1':
+      console.log('case 1')
+      document.querySelector('.home1').textContent = homeScore
+      document.querySelector('.visit1').textContent = visitorScore
+      break
+    case '2':
+      document.querySelector('.home2').textContent = homeScore
+      document.querySelector('.visit2').textContent = visitorScore
+      break
+    case '3':
+      document.querySelector('.home3').textContent = homeScore
+      document.querySelector('.visit3').textContent = visitorScore
+      break
+  }
+
+  const h1 = document.querySelector('.home1').textContent
+  const h2 = document.querySelector('.home2').textContent
+  const h3 = document.querySelector('.home3').textContent
+  const v1 = document.querySelector('.visit1').textContent
+  const v2 = document.querySelector('.visit2').textContent
+  const v3 = document.querySelector('.visit3').textContent
+  console.log('h1 = ' + h1)
+  console.log('h2 = ' + h2)
+  console.log('h3 = ' + h3)
+  console.log('v1 = ' + v1)
+  console.log('v2 = ' + v2)
+  console.log('v3 = ' + v3)
+
+  const hf = parseInt(h1) + parseInt(h2) + parseInt(h3)
+  const vf = parseInt(v1) + parseInt(v2) + parseInt(v3)
+
+  console.log('hf = ' + hf)
+  document.querySelector('.homef').textContent = hf
+  document.querySelector('.visitf').textContent = vf
 }
 
 document.addEventListener('DOMContentLoaded', main)
@@ -103,5 +161,7 @@ document
 document
   .querySelector('.team-2-subtract-1-button')
   .addEventListener('click', team2SubtractPoint)
+
+document.querySelector('.end-period').addEventListener('click', endPeriod)
 
 document.querySelector('.reset').addEventListener('click', reset)
